7cd41ed6f9fecc8556a1a3f8e6735721d7bc256f
root <elijahndaruga@gmail.com>
2022-03-16 13:09:16 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
-/** 
+
+
+/**
  * print_alphabet_x10 - print alphabet 10 times
  *
  * Return: void
  */
 
+
+
 void print_alphabet_x10(void)
+
 {
 int round = 0;
 char letter = 'a';
